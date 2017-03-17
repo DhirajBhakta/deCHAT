64e6d56b2410773dc175716f5fc7710760a35498
Vaibhav Nayak <vaibhav.nayak@gmail.com>
2017-03-17 06:09:21 +0530

    Some more testing & development

--- RServer.py
+++ RServer.py
@@ -1,10 +1,46 @@
+import threading
 import socket
 import json
-
+import sys
 
 #currently using normal dict.
 #TBD: port to database
-import sys
+class RendezvousServer:
+
+	def __init__(self, port):
+		self.serv_port = port
+		self.serv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.serv_sock.bind(("0.0.0.0", self.serv_port))
+		self.peer_table = dict()
+
+	def respondToQuery(self, peer_sock):
+		"""
+		Read a query from the peer socket and respond appropriately
+		Query Format:
+		{"username": "<username>", "local_ip": "<local_ip>", "local_port": "<listening_port>", "query": "<ALL>/<target_username>"}
+		"""
+		query_dict = json.loads(peer_sock.recv(1024))
+		self.peer_table[query_dict['username']] = (query_dict["local_ip"], query_dict["local_port"])
+		if query_dict["query"] == "ALL":
+			resp_json = json.dumps(self.peer_table)
+		elif query_dict["query"]:
+			if self.peer_table.has_key(query_dict["query"]):
+				resp_dict = {query_dict["query"]: self.peer_table[query_dict("query")]}
+				print resp_dict
+				resp_json = json.dumps(resp_dict)
+			else:
+				resp_json = json.dumps(dict())
+		peer_sock.send(resp_json)
+		peer_sock.close()
+
+	def runServer(self):
+		print "Server initialized."
+		self.serv_sock.listen(100)
+		print "Server running on port " + str(self.serv_port) + "...."
+		while True:
+			conn, addr = self.serv_sock.accept()
+			print "Peer at " + str(addr) + " connected..."
+			threading.Thread(target=self.respondToQuery, args=(conn,)).start()
 
 def runServer(port):
 	peers = dict()
@@ -27,4 +63,4 @@ def runServer(port):
 
 if __name__ == "__main__":
 	print("Attempting to initialize server...")
-	runServer(int(sys.argv[1]))
+	RendezvousServer(int(sys.argv[1])).runServer()
--- peer.py
+++ peer.py
@@ -6,35 +6,36 @@ import threading
 import json
 
 class Peer:
-	#recvSock :   Listens for messages.
-	#			  The peer binds to his recvSock. and the recvSock binds to the host's own (IP,port)
-	#			  So , if any outsider wants to send msg to this peer, he(outsider) sends the msg to recvSock of this peer.
-	#			  Hence, recieving messages from random peers is done in a separate Thread.
+	"""
+	recvSock	:	Listens for messages.
+				  	The peer binds to his recvSock. and the recvSock binds to the host's own (IP,port)
+				  	So , if any outsider wants to send msg to this peer, he(outsider) sends the msg to recvSock of this peer.
+				  	Hence, recieving messages from random peers is done in a separate Thread.
 
-	#sendSock :   messages are SENT through sendSock
-	#			  But "WHOM" to send, must be specified.
-	#			  So the peer types "dhiraj:this is my msg" .Then, itll be parsed.,The destination is identified via a table.
-	#				..and the msg "this is my msg" is sent to "dhiraj"'s (IP,PORT)..i.e recvSock of dhiraj'
-	#	So,this peer's input (i.e a message intended to any other specified peer , or an END command) is parsed continuously in a
-	#	separate thread.
+	sendSock	:	messages are SENT through sendSock
+				  	But "WHOM" to send, must be specified.
+				  	So the peer types "dhiraj:this is my msg" .Then, itll be parsed.,The destination is identified via a table.
+					..and the msg "this is my msg" is sent to "dhiraj"'s (IP,PORT)..i.e recvSock of dhiraj'
+					So,this peer's input (i.e a message intended to any other specified peer , or an END command) is parsed
+					continuously in a separate thread.
 
-	#getAllConnectedPeerDetails():   
-	#			 This function contacts the Rendezvous Server, and supplies its own (IP,PORT) to be registered.
-	#			 The R.Server then returns a dict of all registered peers.
-	#			 >>>T.B.D : call this function regularly periodically , to keep track of all connected peers
-	
+	getAllConnectedPeerDetails():
+					 This function contacts the Rendezvous Server, and supplies its own (IP,PORT) to be registered.
+				 	The R.Server then returns a dict of all registered peers.
+				 	>>>T.B.D : call this function regularly periodically , to keep track of all connected peers
+	"""
 	def __init__(self,R_Server_addr,username, self_port):
 		self.username 	   = username
 		self.R_Server_addr = R_Server_addr   #Rendezvous Server address i.e('IP',port) tuple
-		self.my_local_ip   = os.popen('ifconfig wlan0 | grep "inet\ addr" | cut -d: -f2 | cut -d" " -f1').read().strip()
+		self.my_local_ip   = "127.0.0.1"
 		self.my_local_port = self_port  #listening port
 		self.peerMap = {}
 		self.getAllConnectedPeerDetails()
-		
+
 		self.sentinel = True
 		self.recvSock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 		self.recvSock.bind((self.my_local_ip, self.my_local_port))
-		
+
 		self.sendSock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
 		self.msg_recv_thread = threading.Thread(target=self.recieveMessages)
@@ -42,8 +43,8 @@ class Peer:
 
 		self.msg_send_thread = threading.Thread(target=self.sendMessages)
 		self.msg_send_thread.start()
-		
-	
+
+
 
 	#only to receive
 	def recieveMessages(self):
@@ -56,12 +57,12 @@ class Peer:
 				continue
 			if not self.peerMap.has_key(data['USERNAME']):
 				self.peerMap[data['USERNAME']] = (remoteAddr[0],data['PORT'])
-			#pretty-print here,.	
+			#pretty-print here,.
 			print "------- "+data['USERNAME'] +" says :"+data['MSG']+"\n"
 
 
 	#only to Parse input,
-	#and send 
+	#and send
 	def sendMessages(self):
 		print "now waiting for user input..."
 		while(self.sentinel):
@@ -95,13 +96,13 @@ class Peer:
 			except Exception as e:
 				print "WARNING!:Messages are of the form : <USERNAME>:<MESSAGE>"
 				continue
-			
+
 			if(self.peerMap.has_key(recieverID)):
 				self.sendSock.sendto(json.dumps(dataToSend),self.peerMap[recieverID])
 			else:
 				print "!!!ERROR: "+recieverID+" could not be resolved!"
 				print "type HELP for more info."
-				
+
 
 
 	def getAllConnectedPeerDetails(self):
@@ -110,16 +111,16 @@ class Peer:
 		tempSock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 		tempSock.connect(self.R_Server_addr)
 
-		PeerInfo = dict(USERNAME=self.username,IP=self.my_local_ip,PORT=self.my_local_port)
+		PeerInfo = dict(username=self.username, local_ip=self.my_local_ip, local_port=self.my_local_port, query="ALL")
 		tempSock.send(json.dumps(PeerInfo))
-		#now get a dict of all peers,addr 
+		#now get a dict of all peers,addr
 		self.peerMap = json.loads(tempSock.recv(1024*100))
 		for key in self.peerMap.keys():
 			self.peerMap[key] = tuple(self.peerMap[key])
 		tempSock.close()
 
 
-			
+
 
 
 
@@ -146,10 +147,10 @@ if __name__ == '__main__':
 	#Rendezvous Server (IP,port) parsing.
 	R_Server_addr = sys.argv[1].split(':')
 	R_Server_addr[1] = int(R_Server_addr[1])
-	R_Server_addr = tuple(R_Server_addr)	
+	R_Server_addr = tuple(R_Server_addr)
 	selfPort = int(sys.argv[3])
 	username = sys.argv[2]
-	
-	
+
+
 
 	peer = Peer(R_Server_addr, username, selfPort)
